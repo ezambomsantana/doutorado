aa08bfbd4adaff39ce2548fad0444bfa049b31b0
Eduardo Santana <ezambomsantana@gmail.com>
2017-11-30 14:44:16 -0200

    Update trips_count.R

--- simulator_analyses/sbrc/trips_count.R
+++ simulator_analyses/sbrc/trips_count.R
@@ -1,3 +1,4 @@
+
 require("XML")
 require("plyr")
 require("ggplot2")
@@ -6,18 +7,13 @@ require("gridExtra")
 
 library(data.table)
 
-setwd("/home/eduardo/saidas")
-
-xmlfile=xmlParse("trips.xml")
+xmlfile=xmlParse("/home/eduardo/trips.xml")
 
 pointAttribs <- xpathSApply(doc=xmlfile, path="/scsimulator_matrix/trip",  xmlAttrs)
 # TRANSPOSE XPATH LIST TO DF 
 df <- data.frame(t(pointAttribs))
 # CONVERT TO NUMERIC
 df[c('start', 'count')] <- sapply(df[c('start', 'count')],  function(x) as.numeric(as.character(x)))
-
-hist(df$start)
-
 horas <- c(0,3600,7200,10800,14400,18000,21600,25200,28800,32400,36000,39600,43200,46800,50400,54000,57600,61200,64800,68400,72000,75600,79200,82800,86400)
 time <- aggregate(df$count, list(cut(df$start, breaks=horas)), sum)
 
@@ -26,9 +22,10 @@ time$x <- time$x/1000
 
 ps <- data.frame(xspline(time[,1:2], shape=-0.2, lwd=2, draw=F))
 
+sum(time$x)
 theme_set(theme_gray(base_size = 18))
 png('trip_count.png')
 ggplot(data=time, aes(x=horas, y=x, group=1)) +
   geom_bar(stat="identity", fill="#56B4E9") +
-  xlab("Hour of the Day") + ylab("Number of People (x 1000)")
+  xlab("Hora do Dia") + ylab("Número de Viagens (x 1000)")
 dev.off()
