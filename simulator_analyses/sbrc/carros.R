8be0b495f2a3b32b60eb526b54d9b8f536caac39
Eduardo Santana <ezambomsantana@gmail.com>
2017-12-13 20:03:46 -0200

    Update carros.R

--- simulator_analyses/sbrc/carros.R
+++ simulator_analyses/sbrc/carros.R
@@ -1,7 +1,7 @@
 require("ggplot2")
 library(ggpubr)
 
-data <- read.csv("/home/eduardo/events.csv", 
+data <- read.csv("/home/eduardo/events_complete.csv", 
                  header=FALSE, sep = ";", 
                  colClasses= c("integer","character","character","integer","integer","integer"),
                  col.names=c("time","event","vehicle","last_position","duration","distance"))
@@ -9,7 +9,13 @@ data <- read.csv("/home/eduardo/events.csv",
 data_car <- data[data$distance != 0, ]
 data_car$count = 1
 data_car$speed = (data_car$distance / 1000) / ( data_car$duration / 3600)
+#data_car <- data_car[grep("25475", data_car$vehicle, invert = TRUE) ,]
 
+
+
+
+data_car_teste <- data_car[data_car$time > 3600,]
+data_car_teste <- data_car_teste[data_car_teste$time < 10800,]
 
 
 horas <- c(0,3600,7200,10800,14400,18000,21600,25200,28800,32400,36000,39600,43200,46800,50400,54000,57600,61200,64800,68400,72000,75600,79200,82800,86400)
@@ -28,22 +34,22 @@ time$speed <- speed$x
 tempo_medio <- ggplot(time, aes(x = horas)) + 
   geom_line(aes(y = tempo), color="steelblue") +
   xlab("Hora do Dia") + 
-  ylab("Tempo (m)")
+  ylab("Duração Média (m)")
 
 distancia <- ggplot(time, aes(x = horas)) + 
   geom_line(aes(y = distance), color='red') + 
   xlab("Hora do Dia") + 
-  ylab("Distância (km)")
+  ylab("Distância por Viagem (km)")
 
 count <- ggplot(time, aes(x = horas)) + 
   geom_line(aes(y = count), color='green') + 
   xlab("Hora do Dia") + 
-  ylab("Número")
+  ylab("Veículos")
 
 speed_graph <- ggplot(time, aes(x = horas)) + 
   geom_line(aes(y = speed), color='green') + 
   xlab("Hora do Dia") + 
-  ylab("Velocidade")
+  ylab("Velocidade Média (km/h)")
 
 theme_set(theme_gray(base_size = 16))
 png('time_distance_car.png', width = 800, height = 600)
