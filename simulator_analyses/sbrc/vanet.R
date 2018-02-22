9eedafe28c1b6862353ab76fdac2f1e35ad74ef1
Eduardo Santana <ezambomsantana@gmail.com>
2018-02-22 14:54:31 -0300

    Update vanet.R

--- simulator_analyses/sbrc/vanet.R
+++ simulator_analyses/sbrc/vanet.R
@@ -3,20 +3,18 @@ data1 <- read.csv("/home/eduardo/Doutorado/sbrc/test_clean.ns3",
                   colClasses= c("integer","integer","integer","numeric","numeric","character","integer"),
                   col.names=c("time","time_origin","id","lat","long","mac", "number"))
 
-
 time <- aggregate(data1$number, list(data1$time), mean)
 
-
-
 theme_set(theme_gray(base_size = 18))
-png('num_conexoes.png', width = 800, height = 600)
-ggplot(data=time, aes(x=Group.1, y=x, group=1)) +
-  geom_line(stat="identity", color="steelblue") +
-  xlab("Minutos da Simulação") + ylab("Número Médio de Conexões")
+png('num_conexoes.png')
+  ggplot(data=time, aes(x=Group.1, y=x, group=1)) +
+    geom_line(stat="identity", color="steelblue") +
+    xlab("Tempo da Simulação") + ylab("Número Médio de Conexões")
 dev.off()
 
-
-theme_set(theme_gray(base_size = 16))
-png('num_conexoes_box_plot.png', width = 800, height = 600)
-boxplot(data1$number)
+data1$type <- "" 
+theme_set(theme_gray(base_size = 18))
+png('num_conexoes_box_plot.png')
+  ggplot(data1, aes(x=type, y=number)) + geom_boxplot() +
+    ylab("Média de Conexões dos Veículos") + xlab("")
 dev.off()
