274a39e108fd2ef567aed716883d96ec328a79dd
Eduardo Santana <ezambomsantana@gmail.com>
2018-02-22 17:44:05 -0300

    Update buses.R

--- simulator_analyses/bus_model/buses.R
+++ simulator_analyses/bus_model/buses.R
@@ -7,6 +7,9 @@ data_bus <- read.csv("/home/eduardo/events_buses.csv",
                  col.names=c("linha","inicio","fim","tempo","hora_inicio","hora_inicio","hora_tempo"))
 
 data_bus$count = 1
+data_bus$tempo_minuto <- data_bus$tempo / 60
+data_bus$linha <- substr(data_bus$linha, 0, 9)
+data_bus <- data_bus[data_bus$linha == "675K-10-1",]
 
 horas <- c(0,3600,7200,10800,14400,18000,21600,25200,28800,32400,36000,39600,43200,46800,50400,54000,57600,61200,64800,68400,72000,75600,79200,82800,86400)
 time <- aggregate(data_bus$tempo, list(cut(data_bus$fim, breaks=horas)), FUN=mean)
@@ -20,13 +23,22 @@ tempo_medio <- ggplot(time, aes(x = horas)) +
   geom_line(aes(y = tempo), color="steelblue") +
   xlab("Hora do Dia") + 
   ylab("Tempo Médio (m)") +
-  ylim(30, 80)
+  ylim(0, 200)
 
-count <- ggplot(time, aes(x = horas)) + 
-  geom_point(aes(y = tempo), color="steelblue") +
+data_bus$fim <- data_bus$fim / 3600
+data_bus$fim <- floor(data_bus$fim)
+
+count <- ggplot(data_bus, aes(x = fim)) +
+  geom_point(aes(y = tempo_minuto), color="steelblue") +
   xlab("Hora do Dia") + 
   ylab("Tempo Médio (m)") +
-  ylim(30, 80)
+  ylim(50, 150)
+#  ggplot(data_bus, aes(x = fim)) + 
+ # geom_point(aes(y = tempo_minuto, colour=factor(line) )) +
+#  xlab("Hora do Dia") + 
+#  ylab("Tempo Médio (m)") +
+#  ylim(50, 200)
+
 
 theme_set(theme_gray(base_size = 18))
 png('time_distance_bus.png', width = 800, height = 400)
